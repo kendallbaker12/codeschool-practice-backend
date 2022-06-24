aad57f71b52fd912a1061f91d7e8865037cf9d6f
kendall baker <d00262562@dmail.dixie.edu>
2022-06-24 10:37:34 -0600

    added git and post

--- server.js
+++ server.js
@@ -6,8 +6,36 @@ app.use(express.json());
 const Players = require("./persists/hplayer");
 
 app.post("/players", (req, res) => {
+    const vPlayers = req.body;
+    Players.create(vPlayers)
+        .then((player) => {
+            res.json(player);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
 
-})
+app.get("/players/:id", (req, res) => {
+    const id = req.params.id;
+    Players.findById(id)
+        .then((player) => {
+            res.json(player);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
+
+app.get("/players", (req, res) => {
+    Players.find()
+        .then((players) => {
+            res.json(players);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
 
 
 module.exports = app;
